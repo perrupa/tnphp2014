0fafe9e133d2c5e62692b4f73bac9c8ebc949d97
Brian Graham <brian.graham@experiencepoint.com>
2014-10-31 19:40:27 -0400

    Cleans up some default values for personal use

--- slide_config.js
+++ slide_config.js
@@ -1,12 +1,8 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Title Goes Here<br>Up To Two Lines',
+    title: 'For PHP devs with mixed<br>feelings about JavaScript',
     subtitle: 'Subtitle Goes Here',
-    //eventInfo: {
-    //  title: 'Google I/O',
-    //  date: '6/x/2013'
-    //},
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
@@ -22,19 +18,12 @@ var SLIDE_CONFIG = {
 
   // Author information
   presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title<br>Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
-  }/*, {
-    name: 'Second Name',
-    company: 'Job Title, Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
-  }*/]
+    name: 'Brian Graham',
+    company: 'Developer<br>ExperiencePoint',
+    gplus: 'https://twitter.com/brianjgraham',
+    twitter: '@brianjgraham',
+    www: 'https://twitter.com/brianjgraham',
+    github: 'http://github.com/incognito'
+  }]
 };
 
