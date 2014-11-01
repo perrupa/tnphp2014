75f5b18412fffb46555b16f61971319715d571f8
Brian Graham <brian.graham@experiencepoint.com>
2014-10-31 22:04:36 -0400

    Adds first half of presentation.

--- slide_config.js
+++ slide_config.js
@@ -2,7 +2,7 @@ var SLIDE_CONFIG = {
   // Slide settings
   settings: {
     title: 'For PHP devs with mixed<br>feelings about JavaScript',
-    subtitle: 'Subtitle Goes Here',
+    subtitle: '... and how to feel good about it',
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
@@ -19,10 +19,8 @@ var SLIDE_CONFIG = {
   // Author information
   presenters: [{
     name: 'Brian Graham',
-    company: 'Developer<br>ExperiencePoint',
-    gplus: 'https://twitter.com/brianjgraham',
+    company: '',
     twitter: '@brianjgraham',
-    www: 'https://twitter.com/brianjgraham',
     github: 'http://github.com/incognito'
   }]
 };
